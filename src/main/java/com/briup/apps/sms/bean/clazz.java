43eeb006c9fa8c1b770dd8c751276bd2ff3b9a49
1985404546@qq.com <>
2019-07-09 17:05:56 +0800

    【赵笔笔】clazz部分代码编写

---
+++ src/main/java/com/briup/apps/sms/bean/clazz.java
@@ -0,0 +1,28 @@
+package com.briup.apps.sms.bean;
+/*2019/7/9下午17.04
+ * 
+ */
+public class clazz {
+private long id;
+private String name;
+private String begin_time;
+public long getId() {
+	return id;
+}
+public void setId(long id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getBegin_time() {
+	return begin_time;
+}
+public void setBegin_time(String begin_time) {
+	this.begin_time = begin_time;
+}
+}
+
