5ddd4ef94e22554a7103993e82936a6ff6766845
张祯铭 <879736840@qq.com>
2019-07-09 16:38:51 +0800

    [张祯铭]school部分代码编写

---
+++ src/main/java/com/briup/apps/sms/bean/school.java
@@ -0,0 +1,40 @@
+package com.briup.apps.sms.bean;
+
+public class school {
+	private Long id;
+	private String name;
+	private String description;
+	private String tel;
+	private String address;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
