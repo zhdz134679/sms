158c02c5576dacbf29917ce531c76a5f9d93e28f
张文选 <2531346166@qq.com>
2019-07-10 11:48:50 +0800

    [张文选]  提交课程部分代码

---
+++ src/main/java/com/briup/apps/sms/service/CourseService.java
@@ -0,0 +1,12 @@
+package com.briup.apps.sms.service;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.Course;
+
+public interface CourseService {
+   
+	List<Course> selectAll();
+	
+	void saveOrUpdate(Course course) throws Exception;
+}
