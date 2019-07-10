341b1ff1ed0b3205baeb19ae482ad538a20dc65b
章超 <319062702@qq.com>
2019-07-10 17:09:54 +0800

    [章超]测试代码提交

---
+++ src/main/java/com/briup/apps/sms/service/CollegeService.java
@@ -0,0 +1,12 @@
+package com.briup.apps.sms.service;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.College;
+
+public interface CollegeService {
+   
+	List<College> selectAll();
+	
+	void saveOrUpdate(College college) throws Exception;
+}
---
+++ src/main/java/com/briup/apps/sms/service/impl/CollegeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.briup.apps.sms.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.College;
+import com.briup.apps.sms.dao.CollegeDao;
+import com.briup.apps.sms.service.CollegeService;
+
+/**
+ * 逻辑逻辑处理的实现类
+ * */
+@Service
+public class CollegeServiceImpl implements CollegeService {
+	// 依赖注入，实例化CollegeDao并且赋值给CollegeDao这个变量
+	@Resource
+	private CollegeDao collegeDao;
+
+	@Override
+	public List<College> selectAll() {
+		// TODO Auto-generated method stub
+		return collegeDao.selectAll();
+	}
+
+	@Override
+	public void saveOrUpdate(College college) throws Exception {
+		if(college.getId()==null) {
+			collegeDao.insert(college);
+		} else {
+			collegeDao.update(college);
+		}
+		
+	}
+
+}
