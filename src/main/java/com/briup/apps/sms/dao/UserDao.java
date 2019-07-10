e45e76bd68edd858c861104162703a6e7fb27655
赵登泽 <1441355725@qq.com>
2019-07-10 10:37:46 +0800

    [赵登泽]提交配置

---
+++ src/main/java/com/briup/apps/sms/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.briup.apps.sms.dao;
+
+import java.util.List;
+
+import com.briup.apps.sms.bean.User;
+
+public interface UserDao {
+	
+	List<User> selectAll();  //查询所有
+	
+	void insert(User user1);//插入
+	
+	void update(User user1); //更新
+	
+	
+}
