94082d3029c8dff7b12d779e56808a02b4168db7
张若雨 <1493788786@qq.com>
2019-07-10 12:12:07 +0800

    [张若雨]UserRoleimpl部分代码修改

--- src/main/java/com/briup/apps/sms/service/impl/UserRoleimpl.java
+++ src/main/java/com/briup/apps/sms/service/impl/UserRoleimpl.java
@@ -1,5 +1,33 @@
 package com.briup.apps.sms.service.impl;
 
-public class UserRoleimpl {
+import java.util.List;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.briup.apps.sms.bean.UserRole;
+import com.briup.apps.sms.dao.UserRoleDao;
+import com.briup.apps.sms.service.UserRoleService;
+
+@Service
+public class UserRoleimpl implements UserRoleService {
+	@Resource
+	private UserRoleDao UserRoleDao;
+	public List<UserRole> selectAll() {
+		
+		return UserRoleDao.selectAll();
+	}
+	public void saveOrUpdate(UserRole UserRole) throws Exception{
+    	if(UserRole.getId()==null) {
+    		UserRoleDao.insert(UserRole);
+    	}else {
+    		UserRoleDao.update(UserRole);
+    	}
+    }
+	@Override
+	public void saveorUpdate(UserRole UserRole) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
