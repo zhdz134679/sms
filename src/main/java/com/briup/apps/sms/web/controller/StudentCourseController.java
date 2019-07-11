2040bdb1fc1973ead80b1bc026f93871c4c489b3
xiaosheng97 <420299897@qq.com>
2019-07-11 13:08:59 +0800

    【于园皓】更新保存代码提交

--- src/main/java/com/briup/apps/sms/web/controller/StudentCourseController.java
+++ src/main/java/com/briup/apps/sms/web/controller/StudentCourseController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.briup.apps.sms.bean.Clazz;
 import com.briup.apps.sms.bean.StudentCourse;
 import com.briup.apps.sms.service.StudentCourseService;
 
@@ -16,7 +18,17 @@ public class StudentCourseController {
 	
 	@Autowired
 	private StudentCourseService studentCourseService;
-
+	@PostMapping("savOrUpdate")
+	public String saverOrUpdate(StudentCourse studentCourse) {
+		try {
+			studentCourseService.saveOrUpdate(studentCourse);
+			return"保存或更新成功";
+		
+		}catch(Exception e) {
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
 	// http://localhost:8080/school/selectAll
 	@GetMapping("selectAll")
 	public List<StudentCourse> selectAll(){
