dac3d39bb711f7c58997621aa14a4be53c0210f9
章超 <319062702@qq.com>
2019-07-11 15:54:12 +0800

    [章超]呀呀呀呀呀

--- src/main/java/com/briup/apps/sms/web/controller/CollegeController.java
+++ src/main/java/com/briup/apps/sms/web/controller/CollegeController.java
@@ -4,19 +4,33 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.briup.apps.sms.bean.College;
+import com.briup.apps.sms.bean.School;
 import com.briup.apps.sms.service.CollegeService;
 
 @RestController
 @RequestMapping("/College")
 public class CollegeController {
 	
+	
 	@Autowired
 	private CollegeService collegeService;
-
+	@PostMapping("saveOrUpdate")
+	public String saveOrUpdate(College college) {
+		try {
+			collegeService.saveOrUpdate(college);
+			return "保存或更新成功";
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			return e.getMessage();
+		}
+	}
+	
 	// http://localhost:8080/college/selectAll
 	@GetMapping("selectAll")
 	public List<College> selectAll(){
